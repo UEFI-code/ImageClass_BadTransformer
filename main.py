d167211ceaf235ca179d3f0ec6261594da9260fc
UEFI-code <zhuoshizhang@hotmail.com>
2024-03-01 12:52:55 +0800

    Update loss function from L1Loss to MSELoss

--- main.py
+++ main.py
@@ -172,7 +172,7 @@ def main_worker(gpu, ngpus_per_node, args):
 
     # define loss function (criterion), optimizer, and learning rate scheduler
     #criterion = nn.CrossEntropyLoss().cuda(args.gpu)
-    criterion = nn.L1Loss().cuda(args.gpu)
+    criterion = nn.MSELoss().cuda(args.gpu)
 
     optimizer = torch.optim.SGD(model.parameters(), args.lr,
                                 momentum=args.momentum,
