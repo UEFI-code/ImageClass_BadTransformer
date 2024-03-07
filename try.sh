55ae539a3ac373bedd46ec004bb26b5c8a5c6d50
UEFI-code <zhuoshizhang@hotmail.com>
2024-03-07 20:20:12 +0800

    Update main.py command line argument in try.sh

--- try.sh
+++ try.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=4,5,6,7 python main.py --arch RealBadGraphicTransformer -b 256 /storage/nfs/ImageNet
+CUDA_VISIBLE_DEVICES=4,5,6,7 python main.py --arch BadGraphicTransformer -b 256 /storage/nfs/ImageNet
